4a9d4954e76a4bb02d71c2f427c304f24343c5d5
UD016 <ud016jl@gmail.com>
2026-07-09 11:31:56 -0400

    Add test.py
    Initial testing of Agents SDK

---
+++ Code/test.py
@@ -0,0 +1,9 @@
+from agents import Agent, Runner
+
+agent = Agent(name="Assistant", instructions="You are a helpful assistant within the service department")
+
+result = Runner.run_sync(agent, "Which technician should I send on a service call involving a dead battery in Longueuil.")
+print(result.final_output)
+
+
+
